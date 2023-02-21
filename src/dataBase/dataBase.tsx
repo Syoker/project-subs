2c9bdfb32bccd262a60ea7e8e05db1f05e320f90
Syoker <lautarivan@hotmail.com>
2023-02-21 13:30:43 -0300

    deleted test

--- src/dataBase/dataBase.tsx
+++ src/dataBase/dataBase.tsx
@@ -1,5 +1,5 @@
 // for home
-export const seriesTitle = ['Kubo-san wa Mob wo Yurusanai', 'test2', 'test3', 'test4', 'test5', 'test6', 'test7', 'test8', 'test9', 'test10'];
+export const seriesTitle = ['Kubo-san wa Mob wo Yurusanai'];
 export const chaptersTitle = ['Vol. 01 Cap. 001 - La chica heroína y el chico del fondo'];
 export const chaptersDate = ['2023-02-05T11:00:00'];
 export const coversSource = ['112981_1.jpg'];
